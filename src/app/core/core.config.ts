28d11897f8d86d96ba91969c5febfbe74f661226
Robert Jackson <robert.jackson@hotmail.com.au>
2018-03-04 22:35:12 +0930

    Started to design the site and update the styles.

--- src/app/core/core.config.ts
+++ src/app/core/core.config.ts
@@ -11,30 +11,30 @@ function configure(
     const dateFormat = "DD/MM/YYYY";
 
     // Mobishare theme
-    // $mdThemingProvider.definePalette('mobiPrimaryPalette', {
-    //     '50': 'E8EAF6',
-    //     '100': 'C5CAE9',
-    //     '200': '9FA8DA',
-    //     '300': 'e57373',
-    //     '400': 'ef5350',
-    //     '500': 'f44336',
-    //     '600': 'e53935',
-    //     '700': 'd32f2f',
-    //     '800': 'c62828',
-    //     '900': 'b71c1c',
-    //     'A100': 'ff8a80',
-    //     'A200': 'ff5252',
-    //     'A400': 'ff1744',
-    //     'A700': 'd50000',
-    //     'contrastDefaultColor': 'light',
-    //     'contrastDarkColors': ['50', '100',
-    //     '200', '300', '400', 'A100'],
-    //     'contrastLightColors': undefined
-    // });
+    $mdThemingProvider.definePalette('mobiPrimaryPalette', {
+        '50': 'e8eaf6',
+        '100': 'ccddff',
+        '200': '99bbff',
+        '300': '80aaff',
+        '400': '3377ff',
+        '500': '0052F3',
+        '600': '004de6',
+        '700': '0044cc',
+        '800': '003cb3',
+        '900': '002b80',
+        'A100': '4da6ff',
+        'A200': '1a8cff',
+        'A400': '007af5',
+        'A700': '0066cc',
+        'contrastDefaultColor': 'light',
+        'contrastDarkColors': ['50', '100',
+        '200', '300', '400', 'A100'],
+        'contrastLightColors': undefined
+    });
 
     // Angular material theme configuration
     $mdThemingProvider.theme('default')
-        .primaryPalette('indigo')
+        .primaryPalette('mobiPrimaryPalette')
         .accentPalette('cyan', {
             'default': '500' // use shade 200 for default, and keep all other shades the same
         });
